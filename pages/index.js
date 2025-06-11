ef8fe21a7084f11d4eadaf8f45afc04fbf50e9b7
praneeth <gp.gunturu@gmail.com>
2025-06-11 15:43:04 -0500

    setting up the pages layout and a dummy data to render some content for now

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'First meetup',
+        image: 'https://www.usnews.com/object/image/00000195-0240-db6b-afd7-0a7a1da10000/new-main-image-machu-picchu-credit-getty-images.jpg?update-time=1739503747923&size=responsive970',
+        address: 'Egypt'
+    },
+    {
+        id: 'm2',
+        title: 'Second meetup',
+        image: 'https://www.usnews.com/object/image/00000195-0240-db6b-afd7-0a7a1da10000/new-main-image-machu-picchu-credit-getty-images.jpg?update-time=1739503747923&size=responsive970',
+        address: 'Giza'
+    }
+]
+
+export default function Home() {
+    return (
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    )
+}
