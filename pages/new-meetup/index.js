cee1b6916c0cda1db56ee83ccd3b70e63c9b9696
praneeth <gp.gunturu@gmail.com>
2025-06-11 16:26:31 -0500

    adding thr props functions and logging the user entered data

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,15 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
+
+    function onAddMeetup(enteredMeetupData) {
+        console.log(enteredMeetupData)
+    }
+
     return (
         <>
             <div>new meetup</div>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={onAddMeetup} />
         </>
     )
 }
