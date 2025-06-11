d269d4b84f492903a181b2d9188478bb679305e7
praneeth <gp.gunturu@gmail.com>
2025-06-11 17:57:18 -0500

    sending the props for the MeetupDetail component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,6 +2,11 @@ import MeetupDetail from "../../components/meetups/MeetupDetail"
 
 export default function MeetupDetails() {
     return (
-        <MeetupDetail />
+        <MeetupDetail 
+            image="https://www.usnews.com/object/image/00000195-0240-db6b-afd7-0a7a1da10000/new-main-image-machu-picchu-credit-getty-images.jpg?update-time=1739503747923&size=responsive970"
+            title="pyramid"
+            address="Paris"
+            description="Niggas in pars"
+        />
     )
 }
