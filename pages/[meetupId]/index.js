4bf5479e4606eed415b4682bd654856fa26ade7b
praneeth <gp.gunturu@gmail.com>
2025-06-12 15:28:01 -0500

    using the getStaticProps in the dynamic meetupId page and accessing meetupId through the context propd. Here in getStaticProps the context have the params object from which we can access meetupId from the url

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -6,7 +6,24 @@ export default function MeetupDetails() {
             image="https://www.usnews.com/object/image/00000195-0240-db6b-afd7-0a7a1da10000/new-main-image-machu-picchu-credit-getty-images.jpg?update-time=1739503747923&size=responsive970"
             title="pyramid"
             address="Paris"
-            description="Niggas in pars"
+            description="Niggas in paris"
         />
     )
+}
+
+export async function getStaticProps(context) {
+    //Fetching data for a single prop
+
+    const meetupId = context.params.meetupId
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://www.usnews.com/object/image/00000195-0240-db6b-afd7-0a7a1da10000/new-main-image-machu-picchu-credit-getty-images.jpg?update-time=1739503747923&size=responsive970",
+                title: "pyramid",
+                address: "Paris",
+                description: "Niggas in paris"
+            }
+        }
+    }
 }
