73fecd90b04943ea1fc9db1e74d43c9d9c18ed04
praneeth <gp.gunturu@gmail.com>
2025-06-12 16:34:21 -0500

    added getStaticPaths function

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,15 +11,36 @@ export default function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            },
+        ]
+    }
+}
+
 export async function getStaticProps(context) {
     //Fetching data for a single prop
 
     const meetupId = context.params.meetupId
+    console.log(meetupId);
+    
 
     return {
         props: {
             meetupData: {
                 image: "https://www.usnews.com/object/image/00000195-0240-db6b-afd7-0a7a1da10000/new-main-image-machu-picchu-credit-getty-images.jpg?update-time=1739503747923&size=responsive970",
+                id: meetupId,
                 title: "pyramid",
                 address: "Paris",
                 description: "Niggas in paris"
