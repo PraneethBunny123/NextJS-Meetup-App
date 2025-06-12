65d4a4d3eb5942db107a445a900ec9038577cd9a
praneeth <gp.gunturu@gmail.com>
2025-06-11 20:01:31 -0500

    adding getStaticProps function which is an async function in the root index.js to have the pre render data available before the component re-renders. Here the getStaticProps return an object which should have a props object whose value that we want to send as the props. And this props is accepted as props for the main component (Home)

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,16 @@ const DUMMY_MEETUPS = [
     }
 ]
 
-export default function Home() {
+export default function Home(props) {
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
     )
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    }
 }
