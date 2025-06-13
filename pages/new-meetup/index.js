a96dd4cbb7b71ca850cdded8b174d9fdc759df58
praneeth <gp.gunturu@gmail.com>
2025-06-13 16:26:50 -0500

    sending http request to api routes using fetch function

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,19 @@ import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
 
-    function onAddMeetup(enteredMeetupData) {
-        console.log(enteredMeetupData)
+    async function onAddMeetup(enteredMeetupData) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const data = await response.json()
+        
+        console.log(data);
+        
     }
 
     return (
