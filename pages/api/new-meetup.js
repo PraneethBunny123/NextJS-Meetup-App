64211a5dda1f3115c170a420c7802fb38ed63660
praneeth <gp.gunturu@gmail.com>
2025-06-13 15:41:37 -0500

    using MongoClient.connect and adding the connection string to that with modified username and password of our user in the database access

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,11 @@
+import { MongoClient } from "mongodb"
+
 export default function handler(req, res) {
     if(req.method === 'POST') {
         const data = req.body
 
         const {title, image, address, description} = data
+
+        MongoClient.connect('mongodb+srv://bunny:bunny@cluster0.xp1gj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     }
 }
