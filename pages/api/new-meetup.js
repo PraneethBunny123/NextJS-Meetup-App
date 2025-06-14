c52972ddb3cc0ea23887f317da079166afd6fcea
praneeth <gp.gunturu@gmail.com>
2025-06-14 09:07:02 -0500

    created .env.local file to store the secret keys

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,19 +1,27 @@
 import { MongoClient } from "mongodb"
 
 export default async function handler(req, res) {
-    if(req.method === 'POST') {
+    if (req.method === 'POST') {
         const data = req.body
 
-        const client = await MongoClient.connect('mongodb+srv://bunny:bunny@cluster0.xp1gj.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0')
+        const username = process.env.MONGODB_USERNAME
+        const password = process.env.MONGODB_PASSWORD
+        const cluster = process.env.MONGODB_CLUSTER
+        const dbName = process.env.MONGODB_DB
+        const collectionName = process.env.MONGODB_COLLECTION
+
+        const uri = `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`
+
+        const client = await MongoClient.connect(uri)
         const db = client.db()
 
-        const meetupsCollection = db.collection('meetups')
+        const meetupsCollection = db.collection(collectionName)
 
         const result = await meetupsCollection.insertOne(data)
-        console.log(result);
+        console.log(result)
 
         client.close()
-        
-        res.status(201).json({message: 'Meetup inserted!'})
+
+        res.status(201).json({ message: 'Meetup inserted!' })
     }
 }
